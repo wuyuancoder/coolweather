01eee75d785f75d4aef9efc6dabb5baa394f3c8c
ganying <gan.y@aiiage.com>
2018-07-12 16:07:57 +0800

    第二次在android studio上提交

--- app/src/main/java/android/coolweather/com/coolweather/MainActivity.java
+++ app/src/main/java/android/coolweather/com/coolweather/MainActivity.java
@@ -2,12 +2,23 @@ package android.coolweather.com.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        tv = findViewById(R.id.textView);
+        btn.setOnClickListener((v) -> {
+            tv.setText("你好啊！");
+        });
     }
 }
